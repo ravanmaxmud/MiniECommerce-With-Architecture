702836dd357eca38e936650368faa4377ea5c52a
ravanmaxmud <ravanim@code.edu.az>
2023-08-10 17:16:08 +0400

    Aded Folder Structure

---
+++ ECommerceBackEnd/Infrastucture/ECommerceBackEnd.Persistence/ServiceRegistiration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceBackEnd.Application.Abstraction;
+using ECommerceBackEnd.Persistence.Concreats;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackEnd.Persistence
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddPersistenceService(this IServiceCollection services) 
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
