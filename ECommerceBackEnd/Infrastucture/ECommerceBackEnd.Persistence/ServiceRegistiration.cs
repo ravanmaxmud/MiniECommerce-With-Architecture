b77c98ad2e9f51ec6e4a99d75a2cca55e837be58
ravanmaxmud <ravanim@code.edu.az>
2023-08-16 17:03:25 +0400

    Fixed AddScoped Problem in ServiceRegstiration.

--- ECommerceBackEnd/Infrastucture/ECommerceBackEnd.Persistence/ServiceRegistiration.cs
+++ ECommerceBackEnd/Infrastucture/ECommerceBackEnd.Persistence/ServiceRegistiration.cs
@@ -18,15 +18,15 @@ namespace ECommerceBackEnd.Persistence
         public static void AddPersistenceService(this IServiceCollection services) 
         {
             services.AddDbContext<ECommerceDBContext>(options => 
-            options.UseNpgsql(Configurations.ConnectionString),ServiceLifetime.Singleton);
+            options.UseNpgsql(Configurations.ConnectionString));
 
 
-            services.AddSingleton<ICustomerReadRepository,CustomerReadRepository>();
-            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
-            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
-            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
-            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
-            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
 
 
 
--- ECommerceBackEnd/Presentation/ECommerceBackEnd.API/Controllers/ProductController.cs
+++ ECommerceBackEnd/Presentation/ECommerceBackEnd.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceBackEnd.Application.Repositories;
+using ECommerceBackEnd.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,16 +19,23 @@ namespace ECommerceBackEnd.API.Controllers
         }
 
         [HttpGet("GetProducts")]
-        public async void Get()
+        public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new() 
+            await _productWriteRepository.AddRangeAsync(new()
             {
                new() { Id = Guid.NewGuid(), Name = "product 1", Price= 100,CreatedAt=DateTime.UtcNow, Stock=10 },
                new() { Id = Guid.NewGuid(), Name = "product 2", Price= 100,CreatedAt=DateTime.UtcNow, Stock=20 },
                new() { Id = Guid.NewGuid(), Name = "product 3", Price= 100,CreatedAt=DateTime.UtcNow, Stock=30 }
 
             });
-             await _productWriteRepository.SaveAsync();
+            await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id) 
+        {
+           Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
