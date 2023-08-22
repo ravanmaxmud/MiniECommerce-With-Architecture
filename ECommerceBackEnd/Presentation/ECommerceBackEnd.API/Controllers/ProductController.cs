45752df392387c2ce03e44919efe9d004278a389
ravanmaxmud <ravanim@code.edu.az>
2023-08-22 15:26:00 +0400

    Aded Tracking Optimisasion.

--- ECommerceBackEnd/Presentation/ECommerceBackEnd.API/Controllers/ProductController.cs
+++ ECommerceBackEnd/Presentation/ECommerceBackEnd.API/Controllers/ProductController.cs
@@ -21,14 +21,7 @@ namespace ECommerceBackEnd.API.Controllers
         [HttpGet("GetProducts")]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-               new() { Id = Guid.NewGuid(), Name = "product 1", Price= 100,CreatedAt=DateTime.UtcNow, Stock=10 },
-               new() { Id = Guid.NewGuid(), Name = "product 2", Price= 100,CreatedAt=DateTime.UtcNow, Stock=20 },
-               new() { Id = Guid.NewGuid(), Name = "product 3", Price= 100,CreatedAt=DateTime.UtcNow, Stock=30 }
-
-            });
-            await _productWriteRepository.SaveAsync();
+          
         }
 
         [HttpGet("{id}")]
